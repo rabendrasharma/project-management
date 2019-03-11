ff1800a0bb71270bf914ea4b30a56e559ee49b0a
rabendra sharma <rabendrasharma007@gmail.com>
2019-03-11 17:00:38 +0530

    #Feature updated gem to remove server start error

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,6 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require highcharts
-//= require_tree .
 
 $(document).ready(function(){
   $( "#show-profile-options" ).click(function() {
