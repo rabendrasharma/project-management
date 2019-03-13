a875008a4414af708996fe8401f692d428ef24f4
rabendrasharma <rabendrasharma007@gmail.com>
2019-03-13 10:57:11 +0530

    Updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 @admin = User.create(name: "Admin", email: "admin@mail.com", password: "hello123")
-@developer = User.create(name: "Chirag", email: "developer@mail.com", password: "hello123")
+@developer = User.create(name: "developer", email: "developer@mail.com", password: "hello123")
 
 @project = Project.create(owner_id: 1, name: "Defend the Wall", description: "defend the wall from the wildings", due_date: "2016-07-23", status: 0)
 @task = Task.create(project_id: 1, owner_id: 1, name: "Secure the tunnel", description: "flood the tunnel so it can't be be breached by the wildings", due_date: "2016-07-23" )
